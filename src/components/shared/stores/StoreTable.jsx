d875f1ebe481f1b926ae484b4508c801657bfd57
unknown <kajugadaniels@gmail.com>
2025-09-06 18:39:52 +0200

    New chnges

--- src/components/shared/stores/StoreTable.jsx
+++ src/components/shared/stores/StoreTable.jsx
@@ -120,7 +120,7 @@ const StoreTable = ({ rows = [], loading = false, onEdit, onDelete }) => {
                                     <TableCell className="text-right">
                                         <DropdownMenu>
                                             <DropdownMenuTrigger asChild>
-                                                <Button variant="ghost" size="icon" className="h-8 w-8">
+                                                <Button variant="ghost" size="icon" className="h-8 w-8 cursor-pointer">
                                                     <MoreHorizontal className="h-4 w-4" />
                                                 </Button>
                                             </DropdownMenuTrigger>
