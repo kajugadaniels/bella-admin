2e8f54d595c5698eced4ca61af7e2b79ec369209
unknown <kajugadaniels@gmail.com>
2025-09-06 16:03:53 +0200

    Add Sidebar and Header in AppLayout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,13 @@
+import { Header, Sidebar } from '@/components/shared'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
     return (
         <div>
+            <Header />
+            <Sidebar />
             <Outlet />
-            AppLayout
         </div>
     )
 }
