b37d9829ffa1be5d4926a4be12d3944f20a2677d
Dvniels <kajugadaniels@gmail.com>
2025-09-27 17:06:32 +0200

    Add verify page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Login } from './auth/Login'
+export { default as Verify } from './auth/Verify'
 export { default as ForgetPassword } from './auth/ForgetPassword'
 export { default as ResetPassword } from './auth/ResetPassword'
 
