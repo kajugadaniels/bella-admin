ed5435eaf165918d78a958b07d1057853c595652
unknown <kajugadaniels@gmail.com>
2025-10-01 01:08:09 +0200

    orders(ui): add Orders page wrapper

---
+++ src/pages/orders/GetOrders.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderList from '@/components/shared/orders/OrderList';
+
+const GetOrders = () => {
+	return (
+		<div className="space-y-5">
+			<OrderList />
+		</div>
+	);
+};
+
+export default GetOrders;
