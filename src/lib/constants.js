d537431bcfa75554eaea5ad9c980b6d7376669c8
kajugadaniels <kajugadaniels@gmail.com>
2025-11-04 15:20:38 +0200

    chore: move product filter constants to separate file for fast refresh

---
+++ src/lib/constants.js
@@ -0,0 +1,17 @@
+export const DEFAULT_PRODUCT_FILTERS = {
+    category: "",
+    store_id: "",
+    has_store: "",
+    has_remaining: "",
+    is_void: "",
+    min_unit_price: "",
+    max_unit_price: "",
+    expiring_after: "",
+    expiring_before: "",
+    created_after: "",
+    created_before: "",
+};
+
+export const ALL_CATEGORIES_VALUE = "__ALL__";
+export const ALL_STORES_VALUE = "__ALL_STORES__";
+export const GLOBAL_STORE_VALUE = "__GLOBAL__";
