96e88389854cf1a39e617c7c03ddac74fd38b324
kajugadaniels <kajugadaniels@gmail.com>
2025-11-04 15:15:43 +0200

    Fix eslint errors in StockInDetailSheet component

--- src/components/shared/products/StockInDetailSheet.jsx
+++ src/components/shared/products/StockInDetailSheet.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { superadmin } from "@/api";
 
